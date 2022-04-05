f4df6dd3568b3b103c23385ee53dbb5846ca94cf
Maya LARBI <larbim@w.ensma.fr>
2022-04-05 15:04:40 +0200

    Get annotations of project with concept

--- myproject/myapi/views/annotation.py
+++ myproject/myapi/views/annotation.py
@@ -52,16 +52,53 @@ def createAnnotation(request):
             idProject= request.GET.get('idProject')
             D = path + "Annotation"
             try:
-                annotations = db.cypher_query("MATCH (n:owl__NamedIndividual)-[rdf_type]->(f:owl__Class) WHERE f.uri='%s' AND n.projectId='%s' RETURN n.commentaire" % (D,idProject))[0]
+                annotations = db.cypher_query("MATCH (n:owl__NamedIndividual)-[rdf_type]->(f:owl__Class) WHERE f.uri='%s' AND n.projectId='%s' RETURN n.id, n.commentaire" % (D,idProject))[0]
                 max=len(annotations)
                 lis = []
                 for i in range(max):
-                    instance = annotations[i][0]
-                    lis.append(instance)
+                    instanceId = annotations[i][0]
+                    commentaire= annotations[i][1]
+                    # Pour chaque annotation get la relation auquelle elle est rattachée
+                    try:
+                         relation=db.cypher_query("MATCH (n:owl__NamedIndividual)-[rdfs_domain]->(f:owl__NamedIndividual) WHERE f.id='%s' AND NOT(n.label='hasAnnotation') RETURN n.id" % (instanceId))[0]
+                         relation = relation[0][0]
+                         # data = {"id": instanceId, "commentaire": commentaire, "idRelation": relation}
+                         # lis.append(data)
+                         # Get concept
+                         try:
+                             concept = db.cypher_query("MATCH (n:owl__NamedIndividual)-[rdfs_range]-(f:owl__NamedIndividual) WHERE f.id='%s' AND n.label='concept' RETURN n.id" % (relation))[0]
+                             concept=concept[0][0]
+                             # data = {"id":instanceId, "commentaire": commentaire, "idRelation": relation,"idConcept": concept}
+                             # lis.append(data)
+                             try:
+                                 # Get the name of the concept
+                                 conceptName = db.cypher_query("MATCH (n:owl__Class)-[rdf_type]-(f:owl__NamedIndividual) WHERE f.id='%s' RETURN n.uri" % (concept))[0]
+                                 conceptName= conceptName[0][0]
+                                 conceptName=conceptName[len(path): len(conceptName)]
+                                 data = {"id": instanceId, "commentaire": commentaire,"concept":conceptName}
+                                 lis.append(data)
+                             except:
+                                 #Its an instance
+                                 try:
+                                     conceptName = db.cypher_query("MATCH (n:Resource)-[rdf_type]-(f:owl__NamedIndividual) WHERE f.id='%s' RETURN n.uri" % (concept))[0]
+                                     conceptName = conceptName[0][0]
+                                     conceptName = conceptName[len(path): len(conceptName)]
+                                     data = {"id": instanceId, "commentaire": commentaire,"concept": conceptName}
+                                     lis.append(data)
+                                 except:
+                                     print("ERROR GET Instance of Annotation")
+
+                         except:
+                             data = {"id": instanceId, "commentaire": commentaire,"concept":""}
+                             lis.append(data)
+                    except:
+                        # Annotation sans relation | elle doit etre un commentaire libre
+                        data = {"id": instanceId, "commentaire": commentaire,"concept":""}
+                        lis.append(data)
                 response = {"annotation": lis}
                 return JsonResponse(response, safe=False)
             except:
-                response = {"error": "Error occurred"}
+                response = {"error": "Error When Get Annotations of project"}
                 return JsonResponse(response, safe=False)
 
 
